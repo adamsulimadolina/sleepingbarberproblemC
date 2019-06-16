b5a1010ad40de45a07c1a0dbd1001db01404ef70
Adam Sulima Dolina <adam.sulimadolina@gmail.com>
2019-06-16 11:29:13 +0200

    Commented

--- sema+mutex/main.c
+++ sema+mutex/main.c
@@ -13,27 +13,24 @@ struct List
 	struct List *next;
 };
 
-sem_t customer; // 0 - no one in waiting room, >0 - number of customers waiting for haircut
-sem_t barber; // 0 - busy, 1 - free
+sem_t customer; // post when customer comes to waiting room
+sem_t barber; // post when barber finishes haircut
 pthread_mutex_t waitingRoom; // lock waiting room to protect from races
 
-int tmp=0;
+int customersCounter=0; // customers Counter
 int chairs = 10; // number of free chairs in waiting room
 int waitingRoomSize = 10; // number of all chairs in waiting room
-int peopleRejected = 0; // number of ppl who didn't find place in waiting room
-int haircutTime = 2; // time of single haircut (1s --> haircutTime)
-int customerTime = 8; // after customerTime, customer enters waiting room (1 --> customerTime)
+int peopleRejected = 0; // number of peiple who didn't find place in waiting room
 bool debug = false; // boolean variable which allows to write lists
 bool finished = false; // boolean variable which determine if barber finished his work
 int activeCustomer = -1; // variable which contains active customer's id, -1 if no one is active
 
+struct List *rejected = NULL; // list of customers that did not enter waiting room
+struct List *waiting = NULL; // list of customers that entered waiting room
 
-struct List *rejected = NULL;
-struct List *waiting = NULL;
-
-void WriteRejected()
+void WriteList(struct List *start) //function that prints list of customers/rejected people
 {
-	struct List *temp = rejected;
+	struct List *temp = start;
 	printf("\nCustomers that did not enter waiting room: ");
 	while(temp!=NULL)
 	{
@@ -43,19 +40,7 @@ void WriteRejected()
 	printf("\n\n");
 }
 
-void WriteWaiting()
-{
-	struct List *temp = waiting;
-	printf("\nCustomers that are waiting in waiting room: ");
-	while(temp!=NULL)
-	{
-		printf("%d ", temp->customer_id);
-		temp = temp->next;
-	}
-	printf("\n\n");
-}
-
-void PlaceNextRejected(int id)
+void PlaceNextRejected(int id) // places next rejected person on the list
 {
 	struct List *temp = (struct List*)malloc(sizeof(struct List));
 	if(temp == NULL)
@@ -66,10 +51,10 @@ void PlaceNextRejected(int id)
 	temp->customer_id = id;
 	temp->next = rejected;
 	rejected = temp;
-	if(debug==true)WriteRejected();
+	if(debug==true)WriteList(rejected);
 }
 
-void PlaceNextWaiting(int id)
+void PlaceNextWaiting(int id) // places next person in waiting room
 {
 	struct List *temp = (struct List*)malloc(sizeof(struct List));
 	if(temp == NULL)
@@ -80,10 +65,10 @@ void PlaceNextWaiting(int id)
 	temp->customer_id = id;
 	temp->next = waiting;
 	waiting = temp;
-	if(debug==true)WriteWaiting();
+	if(debug==true)WriteList(waiting);
 }
 
-void RemoveCustomer(int id)
+void RemoveCustomer(int id) // removes customer from waiting room, he is getting haircut
 {
 	struct List *temp = waiting;
 	struct List *pop = waiting;
@@ -108,10 +93,10 @@ void RemoveCustomer(int id)
 		pop = temp;
 		temp = temp->next;
 	}
-	if(debug==true)WriteWaiting();
+	if(debug==true)WriteList(waiting);
 }
 
-int Top()
+int Top() // returns id of actual customer
 {
 	struct List *tmp = waiting;
 	while(tmp->next!=NULL)
@@ -124,43 +109,43 @@ int Top()
 void *Customer (void *customer_id)
 {
 	int id = *(int*)customer_id;
-	pthread_mutex_lock(&waitingRoom);
+	pthread_mutex_lock(&waitingRoom); // waiting room lock
 	if(chairs>0)
 	{
 		chairs--;
 		printf("Res: %d WRoom: %d/%d [in: %d] - place in waiting room has been taken.\n", peopleRejected, waitingRoomSize-chairs, waitingRoomSize, activeCustomer);
-		PlaceNextWaiting(id);
-		sem_post(&customer);
-		pthread_mutex_unlock(&waitingRoom);
-		sem_wait(&barber);
+		PlaceNextWaiting(id); // adding customer to queue
+		sem_post(&customer); // signal for barber that he have customer
+		pthread_mutex_unlock(&waitingRoom); // waiting room unlock
+		sem_wait(&barber); // waiting for signal from barber that he finished haircut
 	}
 	else
 	{
 		peopleRejected++;
 		printf("Res: %d WRoom: %d/%d [in: %d] - customer did not enter.\n", peopleRejected, waitingRoomSize-chairs, waitingRoomSize, activeCustomer);
-		PlaceNextRejected(id);
-		pthread_mutex_unlock(&waitingRoom);
+		PlaceNextRejected(id); // adding person to rejected list
+		pthread_mutex_unlock(&waitingRoom); // waiting room unlock
 	}
 }
 
 void *Barber()
 {
-	while(!finished)
+	while(!finished) // while he did not finished job for today
 	{
-		sem_wait(&customer);
+		sem_wait(&customer); // waiting for signal, that he has ready customer
 		if(!finished){
-			pthread_mutex_lock(&waitingRoom);
-			activeCustomer = Top();
-			tmp++;
-			chairs++;
+			pthread_mutex_lock(&waitingRoom); // waiting room lock
+			activeCustomer = Top(); // setting active customer to first from top of the queue
+			customersCounter++;
+			chairs++; // frees one of the waiting room chairs
 			printf("Res: %d WRoom: %d/%d [in: %d] - getting haircut.\n", peopleRejected, waitingRoomSize-chairs, waitingRoomSize, activeCustomer);
-			sem_post(&barber);
-			pthread_mutex_unlock(&waitingRoom);
-			RemoveCustomer(activeCustomer);
+			sem_post(&barber); // signal that he finished haircut
+			pthread_mutex_unlock(&waitingRoom); // awaiting room unlock
+			RemoveCustomer(activeCustomer); // removing customer from waiting room queue
 		}
 		else
 		{
-			printf("\nBarber is going to his home. \nToday he earned some money, because he did haircut for %d people.\n",tmp);
+			printf("\nBarber is going to his home. \nToday he earned some money, because he did haircut for %d people.\n",customersCounter);
 		}
 	}
 }
@@ -204,22 +189,13 @@ int main(int argc, char *argv[])
 		perror("\n\nEXIT -> Can't allocate memory for customer threads");
 		exit(EXIT_FAILURE);
 	}
-	if(customersThreads==NULL)
-	{
-		perror("\n\nEXIT -> Can't allocate memory for customer threads.");
-		exit(EXIT_FAILURE);
-	}
 
 	if((array = malloc(sizeof(int)*numberOfCustomers))==NULL)
 	{
 		perror("\n\nEXIT -> Can't allocate memory for threads array.");
 		exit(EXIT_FAILURE);
 	}
-	if(array == NULL)
-	{
-		perror("\n\nEXIT -> Can't allocate memory for threads array.");
-		exit(EXIT_FAILURE);
-	}
+
 	int i;
 	for(i=0; i<numberOfCustomers; i++)
 	{
